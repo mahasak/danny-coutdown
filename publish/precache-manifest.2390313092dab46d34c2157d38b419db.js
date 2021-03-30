7c0e4fee9c7cc73dbc02c6a03a82ea84c4fe8278
Max Pijittum <mpijittum@fb.com>
2021-03-31 00:07:54 +0800

    add publish folder

---
+++ publish/precache-manifest.2390313092dab46d34c2157d38b419db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97856364dd0da378ab08ee3ea519987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e42bd4a4f83211b914c",
+    "url": "/static/css/main.288b75de.chunk.css"
+  },
+  {
+    "revision": "1653aa0705b514076a88",
+    "url": "/static/js/2.2acb32a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2acb32a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e42bd4a4f83211b914c",
+    "url": "/static/js/main.39771b35.chunk.js"
+  },
+  {
+    "revision": "bf28fddfc099d65d2368",
+    "url": "/static/js/runtime-main.e8c2719e.js"
+  },
+  {
+    "revision": "c693aef2f6df064984009e91cc61817c",
+    "url": "/static/media/baby-newborn.c693aef2.svg"
+  }
+]);
